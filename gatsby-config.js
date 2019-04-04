4a1964ddfe3e20c8dce3da49deb3a77da7831966
Skillz <gankstarsleague@gmail.com>
2019-04-03 21:38:16 -0400

    updating config file for site title and stuf

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Guns of Glory Tracker`,
+    description: `Track all your upgrades with your entire alliance to be the best!`,
+    author: `@Skillz4Killz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
