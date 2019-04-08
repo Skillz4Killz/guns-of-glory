eb915d14c1c26113648569c6f026d5b282c56d7a
Skillz <gankstarsleague@gmail.com>
2019-04-08 18:27:20 -0400

    adding profile

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -4,15 +4,7 @@ import { login, logout, isAuthenticated, getProfile } from "../services/auth"
 import { Link } from "gatsby"
 import Profile from "../components/profile"
 import Layout from "../components/layout"
-// {
-//   "sub": "auth0|5cabc1ad782bb20ecbc43bce",
-//   "nickname": "test",
-//   "name": "test@gmail.com",
-//   "picture": "https://s.gravatar.com/avatar/1aedb8d9dc4751e229a335e371db8058?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fte.png",
-//   "updated_at": "2019-04-08T21:48:29.838Z",
-//   "email": "test@gmail.com",
-//   "email_verified": false
-// }
+
 const Account = () => {
   if (!isAuthenticated()) {
     login()
@@ -20,7 +12,7 @@ const Account = () => {
   }
 
   const user = getProfile()
-
+  if (user) console.log("Success!")
   return (
     <Layout>
       <nav>
@@ -35,7 +27,6 @@ const Account = () => {
           Log Out
         </a>
       </nav>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
       <Router>
         <Profile path="/profile/" />
       </Router>
