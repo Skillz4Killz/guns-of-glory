87f30c56d6f7138623f47c3d433a84d8239ebe3e
Angelo Cantone <angelocantone@imac-di-angelo.homenet.telecomitalia.it>
2019-04-08 22:39:29 +0200

    Minor fixes

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { getUser } from "../services/auth"
 import Tracker from "../components/tracker"
+import StickyFooter from 'react-sticky-footer'
 
 import academy from "../constants/buildings/academy"
 const Profile = () => (
@@ -12,6 +13,30 @@ const Profile = () => (
     </ul>
 
     <Tracker building={academy} />
+    <footer
+      className="footer"
+      bottomThreshold={50}
+      style={{
+        backgroundColor: '#050E19',
+        padding: '1rem',
+        fontSize: '14px',
+        fontFamily:'Helvetica',
+        color: 'white',
+        textAlign: 'center',
+        bottom: '0px',
+        position: 'fixed',
+        width: '100%'
+      }}
+    >
+      <p>© {new Date().getFullYear()}, Copyright GoGHub</p>
+      <p>
+        GoGHub is not affiliated or part of Guns of Glory. All Guns of
+        Glory copyrights and art assets belong to Guns of Glory.
+            </p>
+      Made by <a href="https://discord.gg/rWMuMdk">Skillz4Killz</a> and
+            {"  "}
+      <a href="https://twitter.com/AngeloCant1">AngeloC</a>
+    </footer>
   </>
 )
 
