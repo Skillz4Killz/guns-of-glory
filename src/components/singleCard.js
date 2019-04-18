87de3ab2b75c97e18848c1ab2239eacbf4ef7674
Skillz <gankstarsleague@gmail.com>
2019-04-18 11:25:56 -0400

    more fixes

--- src/components/singleCard.js
+++ src/components/singleCard.js
@@ -32,7 +32,29 @@ const Card = props => (
       </div>
 
       <div>
-        <p>back</p>
+        <img src={props.image} alt={props.text} className="cardImage" />
+        <div className="badge">{props.level}</div>
+        <h3 className="buildingName">{props.name}</h3>
+        <p className="resourcesLeft">{props.text}</p>
+        <div className="resources">
+          {props.resources.map((resource, index) => {
+            return resource.amount ? (
+              <div key={index}>
+                <p className="resources">{resource.amount}</p>
+                <p className="resourceName">{resource.type}</p>
+              </div>
+            ) : null
+          })}
+        </div>
+        <div className="levelup">
+          <div className="levelupIconDiv">
+            <h1 className="minus">-</h1>
+          </div>
+          <p className="levelupText">Level {props.level}</p>
+          <div className="levelupIconDiv">
+            <h1 className="plus">+</h1>
+          </div>
+        </div>
       </div>
     </ReactCardFlipper>
   </div>
