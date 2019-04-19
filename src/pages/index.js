c3265ae1333fae40109ed5e865c89fffc0a6001a
Skillz <gankstarsleague@gmail.com>
2019-04-19 19:27:38 -0400

    forgot to push changes on index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,36 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <Link to="/profile/">Login to to your account</Link>
+    <div>
+      <div className='banner'></div>
+      <h1 style={{ color: 'white', fontSize: '24px', position: 'relative', marginTop: '32px', textAlign: 'center' }}>Your Profile Page</h1>
+      <Tabs>
+        <TabList>
+          <Tab>Overview</Tab>
+          <Tab>Inner</Tab>
+          <Tab>Outer</Tab>
+          <Tab>Army</Tab>
+          <Tab>Guards</Tab>
+        </TabList>
+        <TabPanel>
+          <Overview player={player} />
+        </TabPanel>
+        <TabPanel>
+          <Tracker player={player} category="inner" />
+        </TabPanel>
+        <TabPanel>
+          <Tracker player={player} category="outer" />
+        </TabPanel>
+        <TabPanel>
+          <ComingSoon />
+        </TabPanel>
+        <TabPanel>
+          <ComingSoon />
+        </TabPanel>
+      </Tabs>
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+    <SignOut />
   </Layout>
 )
 
