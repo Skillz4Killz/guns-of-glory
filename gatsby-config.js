a9ca09585179b006c19541f6e4728ccdde9c26f8
Angelo Cantone <angelocantone@imac-di-angelo.homenet.telecomitalia.it>
2019-04-21 00:35:39 +0200

    THEME COLOR

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "gatsby-starter-firebase",
+        name: 'GOGHUB',
+        short_name: 'GOGHUB',
         short_name: "starter",
         start_url: "/",
-        background_color: "#ffb200",
-        theme_color: "#663399",
-        display: "minimal-ui",
+        background_color: "#bc9060",
+        theme_color: "#bc9060",
+        display: 'fullscreen',
         icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
       },
     },
