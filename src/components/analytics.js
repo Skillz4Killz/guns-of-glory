b3f9b4c8b9a6d66ec1e39364db62028648e01d9c
Skillz <gankstarsleague@gmail.com>
2019-04-20 11:04:14 -0400

    preparing analytics

--- src/components/analytics.js
+++ src/components/analytics.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { resourceAnalytics } from "../utils/utils"
-// import FoodImage from "../images/food.png"
-// import IronImage from "../images/iron.png"
-// import SilverImage from "../images/silver.png"
-// import WoodImage from "../images/wood.png"
+import FoodImage from "../images/food.png"
+import IronImage from "../images/iron.png"
+import SilverImage from "../images/silver.png"
+import WoodImage from "../images/wood.png"
 
 export default (props) => {
 	const data = resourceAnalytics(props.buildings);
@@ -13,8 +13,20 @@ export default (props) => {
 	}
 	return (
 		<div style={inlineStyle}>
-			<h1>Total Resources Remaining: Food {data.left.food} Wood {data.left.wood} Iron {data.left.iron} Silver {data.left.silver}</h1>
-			<h1>Total Resources Spent: Food {data.spent.food} Wood {data.spent.wood} Iron {data.spent.iron} Silver {data.spent.silver}</h1>
+			<div>
+				<h1>Total Resources Remaining:</h1>
+				<div><img src={FoodImage} alt="food" /><p>{data.left.food}</p></div>
+				<div><img src={WoodImage} alt="food" /><p>{data.left.wood}</p></div>
+				<div><img src={IronImage} alt="food" /><p>{data.left.iron}</p></div>
+				<div><img src={SilverImage} alt="food" /><p>{data.left.silver}</p></div>
+			</div>
+			<div>
+				<h1>Total Resources Spent:</h1>
+				<div><img src={FoodImage} alt="food" /><p>{data.spent.food}</p></div>
+				<div><img src={WoodImage} alt="food" /><p>{data.spent.wood}</p></div>
+				<div><img src={IronImage} alt="food" /><p>{data.spent.iron}</p></div>
+				<div><img src={SilverImage} alt="food" /><p>{data.spent.silver}</p></div>
+			</div>
 		</div>
 	)
 }
